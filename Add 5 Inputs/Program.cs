25bd764b13bd712c17102c53568778431f04e6ef
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 03:56:17 +0500

    Add files via upload
    A simple program to take 5 inputs from the user and Add them.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other software(Visual Studio etc) and run.

---
+++ Add 5 Inputs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int c, d, e;
+        string a;
+
+        d = 0;
+        e = 0;
+
+        do
+        {
+            Console.Write("Input = ");
+
+            a = Console.ReadLine();
+
+            c = Convert.ToInt32(a);
+            d = d + c;
+            e++;
+        }
+        while (e != 5);
+
+        Console.WriteLine("Ans = " + d);
+    }
+}
