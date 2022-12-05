35e09f038cd48de7151abc71b3e1a18c271d0e4d
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 04:40:05 +0500

    Add files via upload
    A simple program to take input from user and show whether the input is an Even number Or an Odd number until the user enters -1.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Even OR Odd -1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int b;
+
+        do
+        {
+            Console.Write("Input = ");
+
+            string a = Console.ReadLine();
+
+            b = Convert.ToInt32(a);
+
+            if (b % 2 == 0)
+
+                Console.WriteLine("Input = Even");
+
+            else
+
+                Console.WriteLine("Input = Odd");
+        }
+        while (b != -1);
+    }
+}
