bd8123b66f3463d557d79829b06c3925140b8b9c
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 04:33:11 +0500

    Add files via upload
    A simple program to take input from user and print all Even and Odd numbers up to that input.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Even & Odd Up to Input/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int b;
+
+        Console.Write("Input = ");
+
+        string a = Console.ReadLine();
+
+        b = Convert.ToInt32(a);
+
+        do
+        {
+            if (b % 2 == 0)
+
+                Console.WriteLine(b + " = Even");
+
+            else
+
+                Console.WriteLine(b + " = Odd");
+
+            b = b - 1;
+        }
+        while (b != 0);
+    }
+}
