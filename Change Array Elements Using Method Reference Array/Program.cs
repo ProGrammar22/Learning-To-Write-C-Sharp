117fc305d5c34c8586159b309158a725d64bfca4
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 04:28:23 +0500

    Add files via upload
    A simple program to assign new values to elements of an array.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Change Array Elements Using Method Reference Array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 4, 5 };
+
+        Console.WriteLine("Array = " + arr[0] + "," + arr[1]);
+
+        Change(ref arr);
+
+        Console.WriteLine("Array = " + arr[0] + "," + arr[1]);
+    }
+    static void Change(ref int[] arr)
+    {
+        Console.Write("Choices : 1, 2\n Input : ");
+
+        string a = Console.ReadLine();
+        int b = Convert.ToInt32(a);
+
+        if (b == 1)
+        {
+            Console.Write("Change " + arr[0] + " to : ");
+
+            a = Console.ReadLine();
+            b = Convert.ToInt32(a);
+
+            arr[0] = b;
+        }
+        else if (b == 2)
+        {
+            Console.Write("Change " + arr[1] + " to : ");
+
+            a = Console.ReadLine();
+            b = Convert.ToInt32(a);
+            arr[1] = b;
+        }
+        else
+        {
+            Console.Write("Wrong Input\n");
+        }
+    }
+}
