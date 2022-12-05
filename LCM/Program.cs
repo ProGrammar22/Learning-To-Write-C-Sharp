1d1e67438dbd790030906527d2476f8b4bab3931
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 04:54:11 +0500

    Add files via upload
    A program to show LCM of two numbers after taking input from user.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ LCM/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int c, d, e, f;
+        string a, b;
+
+        d = 0;
+        e = 0;
+
+        Console.Write("Input = ");
+
+        a = Console.ReadLine();
+
+        c = Convert.ToInt32(a);
+
+        Console.Write("Input = ");
+
+        b = Console.ReadLine();
+
+        f = Convert.ToInt32(b);
+
+        if (c > f)
+
+            d = c;
+
+        else
+        {
+            d = f;
+        }
+        for (e = d; ; e += d)
+        {
+            if (e % c == 0 && e % f == 0)
+            {
+                Console.WriteLine("LCM = " + e); break;
+            }
+        }
+    }
+}
