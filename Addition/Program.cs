440cef2dcde5d128ca57bc74486670e9e4cb9bf7
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 04:08:33 +0500

    Add files via upload
    A simple program to Take 2 inputs and Add them.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other software(Visual Studio etc) and run.

---
+++ Addition/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int c, d, e;
+        string a, b;
+
+        Console.Write("Input First Number: ");
+
+        a = Console.ReadLine();
+
+        Console.Write("Input Second Number: ");
+
+        b = Console.ReadLine();
+
+        c = Convert.ToInt32(a);
+        d = Convert.ToInt32(b);
+        e = c + d;
+
+        Console.WriteLine("Ans = " + e);
+    }
+}
