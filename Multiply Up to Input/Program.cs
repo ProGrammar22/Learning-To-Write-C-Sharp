ae8eae23a45363729690ae927edbe35a3e7383b2
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 04:57:27 +0500

    Add files via upload
    A program to take input from user, Multiply all numbers up to that input and print the answer.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Multiply Up to Input/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int b, c;
+
+        Console.Write("Input = ");
+
+        string a = Console.ReadLine();
+
+        b = Convert.ToInt32(a);
+        c = 1;
+
+        do
+        {
+            c = c * b;
+            b--;
+        }
+        while (b > 1);
+
+        Console.Write("Ans = " + c);
+    }
+}
