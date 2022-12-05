98414d5a8f2ddb4edf33cb472812b9e653042ac4
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 04:10:42 +0500

    Add files via upload
    A simple program to take input in Fahrenheit and Convert it into Celsius.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Celsius to Fahrenheit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        double f;
+
+        Console.Write("Input = ");
+
+        string c = Console.ReadLine();
+
+        f = Convert.ToDouble(c);
+
+        f = (f * 1.8) + 32;
+
+        Console.WriteLine(f + " °F");
+    }
+}
