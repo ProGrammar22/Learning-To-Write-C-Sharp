78640cb23d72e6e4c50fd90f809235ddcaa05b4f
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 04:59:25 +0500

    Add files via upload
    A simple program to take input from user, Multiply 2 numbers using a method and print the answer.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Multiply Using Method/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        Console.Write("First Input : ");
+
+        string a = Console.ReadLine();
+        int b = Convert.ToInt32(a);
+
+        Console.Write("Second Input : ");
+
+        a = Console.ReadLine();
+        int c = Convert.ToInt32(a);
+
+        Console.Write("Answer = " + Print(b, c));
+    }
+    static int Print(int b, int c)
+    {
+        int i = b * c;
+
+        return i;
+    }
+}
