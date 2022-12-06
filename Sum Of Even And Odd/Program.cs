86ba158fa8b0ea48b0f931b4666fe71063955510
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 05:13:33 +0500

    Add files via upload
    A program to print Sum of all Even and Sum of all Odd numbers up to an input number.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Sum Of Even And Odd/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int b, c, d;
+
+        c = 0;
+        d = 0;
+
+        Console.Write("Input = ");
+
+        string a = Console.ReadLine();
+
+        b = Convert.ToInt32(a);
+
+        do
+        {
+            if (b % 2 == 0)
+            {
+                c = c + b;
+            }
+            else
+            {
+                d = d + b;
+            }
+            b--;
+        }
+        while (b > 0);
+
+        Console.WriteLine("Sum of Even = " + c);
+
+        Console.WriteLine("Sum of Odd = " + d);
+    }
+}
