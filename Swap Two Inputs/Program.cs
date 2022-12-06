d42a64b53c5352c002e31adeee0f356df17f64b8
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 05:15:59 +0500

    Add files via upload
    A very simple program to swap two input numbers.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Swap Two Inputs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int b, c, d;
+        string a, e;
+
+        Console.Write("First Input = ");
+
+        a = Console.ReadLine();
+
+        b = Convert.ToInt32(a);
+
+        Console.Write("Second Input = ");
+
+        e = Console.ReadLine();
+
+        c = Convert.ToInt32(e);
+
+        d = c;
+        c = b;
+        b = d;
+
+        Console.WriteLine("Fisrst Input = " + b);
+        Console.WriteLine("Second input = " + c);
+    }
+}
