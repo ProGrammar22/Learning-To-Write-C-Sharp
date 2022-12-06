31d71243ef72e08b0ad1ba483208b649ee3daec7
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 05:01:25 +0500

    Add files via upload
    A program to Multiply two numbers using Reference in a Method.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Multiply Using Method Reference/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int result = 0;
+
+        Console.Write("First Input = ");
+
+        string a = Console.ReadLine();
+        int b = Convert.ToInt32(a);
+
+        Console.Write("Second Input = ");
+
+        a = Console.ReadLine();
+        int c = Convert.ToInt32(a);
+
+        Print(b, c, ref result);
+
+        Console.WriteLine("Answer = " + result);
+    }
+    static void Print(int b, int c, ref int result)
+    {
+        result = b * c;
+    }
+}
