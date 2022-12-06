d0c0aa5ed223fa5df6a0236965a642870ee92219
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 05:14:55 +0500

    Add files via upload
    A simple program to print Sum of all numbers up to an input number.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Sum Up to Input/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int b, c;
+
+        Console.Write("Input = ");
+
+        string a = Console.ReadLine();
+
+        b = Convert.ToInt32(a);
+        c = 0;
+
+        do
+        {
+            c = c + b;
+            b = b - 1;
+        }
+        while (b > 0);
+
+        Console.Write("Ans = " + c);
+    }
+}
