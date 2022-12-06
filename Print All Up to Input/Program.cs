29042ba7d3ae4acf17c9fdbbfbef2622b06612ec
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 05:06:12 +0500

    Add files via upload
    A simple program to print all numbers up to the input number.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Print All Up to Input/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int b, c;
+
+        Console.Write("Input = ");
+
+        string a = Console.ReadLine();
+
+        b = Convert.ToInt32(a);
+        c = 0;
+
+        do
+        {
+            c++;
+            Console.WriteLine("Output = " + c);
+        }
+        while (c != b);
+    }
+}
