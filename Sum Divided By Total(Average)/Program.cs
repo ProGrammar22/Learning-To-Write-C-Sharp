1556a4c10aa64baf6ae7c93e975443ef6945c833
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 05:10:46 +0500

    Add files via upload
    A simple program to take input from user and calculate Average of all input numbers before the user enters -1.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Sum Divided By Total(Average)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        decimal b, c, d;
+        string a;
+
+        c = 0;
+        d = 0;
+
+        do
+        {
+            Console.Write("Input = ");
+
+            a = Console.ReadLine();
+
+            b = Convert.ToDecimal(a);
+
+            if (b == -1) break;
+            {
+                c = c + b;
+                d++;
+            }
+        }
+        while (b != -1);
+
+        b = c / d;
+
+        Console.Write("Ans = " + b);
+    }
+}
