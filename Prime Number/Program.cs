4c3e18e0fe44cc5a08ac4dec3cee5b3232dd89ab
ProGrammar22 <119873698+ProGrammar22@users.noreply.github.com>
2022-12-06 05:04:44 +0500

    Add files via upload
    A simple program to print whether the input number is a Prime number or not.
    
    How to run:
    
    1. Download the folder
    2. Open the folder in Visual Studio Code
    3. Run in Visual Studio Code through Terminal OR Copy the code and paste it in any other Software(Visual Studio etc) and run.

---
+++ Prime Number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        int b, c;
+
+        Console.Write("Input = ");
+
+        string a = Console.ReadLine();
+
+        b = Convert.ToInt32(a);
+        c = 1;
+
+        do
+        {
+            c++;
+
+            if (b % c == 0)
+            {
+                Console.WriteLine(b + " is not a Primr Number"); break;
+            }
+        }
+        while (c < b);
+        {
+            if (b % c != 0)
+            {
+                Console.WriteLine(b + " is a Primr Number");
+            }
+        }
+    }
+}
